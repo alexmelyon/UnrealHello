a0f111e5d8b273c2ae457523c481ef3ddf31ca25
Aleksandr M <alexmelyon@gmail.com>
2021-08-31 02:08:22 +0500

    28

--- Source/UnrealHello/Private/BaseGeometryActor.cpp
+++ Source/UnrealHello/Private/BaseGeometryActor.cpp
@@ -2,6 +2,9 @@
 
 
 #include "BaseGeometryActor.h"
+#include "Engine/Engine.h"
+
+DEFINE_LOG_CATEGORY_STATIC(LogBaseGeometry, All, All)
 
 // Sets default values
 ABaseGeometryActor::ABaseGeometryActor()
@@ -17,6 +20,7 @@ void ABaseGeometryActor::BeginPlay()
 	Super::BeginPlay();
 
 	printTypes();
+	printStringTypes();
 }
 
 // Called every frame
@@ -32,3 +36,15 @@ void ABaseGeometryActor::printTypes()
 	UE_LOG(LogTemp, Warning, L"BOOL %d", true);
 }
 
+void ABaseGeometryActor::printStringTypes() {
+
+	FString weapons = "Weapons=" + FString::FromInt(5);
+	FString health = "Health=" + FString::SanitizeFloat(1.23F);
+	bool isDead = false;
+	FString isDeadStr = "Is dead=" + FString(isDead ? "true" : "false");
+	FString stat = FString::Printf(TEXT("All %s %s %s"), *weapons, *health, *isDeadStr);
+	UE_LOG(LogBaseGeometry, Warning, L"%s", *stat);
+
+	GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Red, FString("John Connor"));
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Green, stat, true, FVector2D(1.5F));
+}
